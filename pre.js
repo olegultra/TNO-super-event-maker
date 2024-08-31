f5fa42667fb488c1fa46db41d983f1ad2ad7f17f
olegultra <147877661+olegultra@users.noreply.github.com>
2024-08-31 12:05:39 +0300

    Update pre.js

--- pre.js
+++ pre.js
@@ -144,7 +144,7 @@ body {
             {
                 type: "text", text: "Event Title",
                 x: 194, y: -4,
-                width: 500,
+                width: 200,
                 classes: ["TNO-info-text"], style: "font-size: 20px; text-align: center;",
                 replaceable: true
             }
